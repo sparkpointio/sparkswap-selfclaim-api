a26e5ab1adf71ecb8fdddc589ce7800971e41e10
Aldrick Bonaobra <git@aldrickb.com>
2023-07-18 14:28:58 +0800

    Add scripts for merkle root

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import express from 'express';
+import express, { Request, Response }  from 'express';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
+});
+
+app.post('/api/endpoint', (req: Request, res: Response) => {
+    const { name, email } = req.body;
+  
+    // Process the input data here
+    // ...
+  
+    res.status(200).json({ message: 'Input received successfully' });
 });
 
 app.listen(port, () => {
