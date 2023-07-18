37b657cbf4efa80571e202a5846a8f4f3f7776d4
Aldrick Bonaobra <git@aldrickb.com>
2023-07-18 14:32:09 +0800

    Add cors and vercel config

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ import express, { Request, Response }  from 'express';
 const app = express();
 const port = 3000;
 
+app.use((req: Request, res: Response, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
