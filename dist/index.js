ac5bfd0e873a318a0d95bd84e9734156e33757ee
Aldrick Bonaobra <git@aldrickb.com>
2023-07-19 08:55:46 +0800

    Add merkle endpoint

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const parse_balance_map_1 = require("./parse-balance-map");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+app.use(express_1.default.json());
+app.post('/api/merkle', (req, res) => {
+    const { recipient, tokenDecimal } = req.body;
+    let json = {};
+    try {
+        for (let i = 0; i < recipient.length; i++) {
+            if (json[recipient[i].address] != undefined)
+                throw new Error('Duplicate address');
+            json[recipient[i].address] = (Math.ceil(recipient[i].amount * (Math.pow(10, tokenDecimal)))).toString(16);
+        }
+        res.status(200).send(JSON.stringify((0, parse_balance_map_1.parseBalanceMap)(json)));
+    }
+    catch (e) {
+        return res.status(400).send(e);
+    }
+});
 app.get('/', (_req, res) => {
     return res.send('Express Typescript on Vercel');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
 import express, { Request, Response } from 'express'
+import { parseBalanceMap } from './parse-balance-map'
 
 const app = express()
 const port = process.env.PORT || 8080
+
+app.use(express.json());
+
+app.post('/api/merkle', (req, res) => {
+    const { recipient, tokenDecimal } = req.body;
+
+    let json: any = {};
+
+    try {
+        for (let i = 0; i < recipient.length; i++) {
+            if (json[recipient[i].address] != undefined) throw new Error('Duplicate address');
+            json[recipient[i].address] = (Math.ceil(recipient[i].amount * ( 10 ** tokenDecimal ))).toString(16);
+        }
+
+        res.status(200).send(JSON.stringify(parseBalanceMap(json)))
+    }
+    catch (e) {
+        return res.status(400).send(e);
+    }
+  
+    
+});
 
 app.get('/', (_req: Request, res: Response) => {
     return res.send('Express Typescript on Vercel')
